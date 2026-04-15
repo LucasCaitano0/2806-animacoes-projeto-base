c9b929fb114c8b7f055ba8c740270de766081134
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2026-04-15 17:02:40 -0300

    animação do card

---
+++ src/app/animations.ts
@@ -0,0 +1,33 @@
+import { animate, style, transition, trigger, state } from '@angular/animations';
+
+export const highlightedStateTrigger = trigger('highlightedState', [
+    state('default', style({
+      border: '2px solid #B2B6FF'
+    })),
+    state('highlighted', style({
+      border: '4px solid #B2B6FF',
+      filter: 'brightness(92%)'
+    })),
+    transition('default => highlighted', [
+      animate('200ms esse-out', style({
+        transform: 'scale(1.02)'
+      })),
+      animate(200)
+    ])
+  ])
+
+  export const showStateTrigger = trigger('shownState', [
+    transition(':enter', [
+        style({
+            opacity: 0
+        }),
+        animate(300, style ({
+            opacity: 1
+        }))
+    ]),
+    transition(':leave', [
+        animate(300, style({
+            opacity: 0
+        }))
+    ])
+  ])
