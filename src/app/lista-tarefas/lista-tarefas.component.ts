6a4c639fce099cb351e570c02f8d13444c89788d
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2026-04-16 16:52:43 -0300

    projeto finalizado

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,13 +4,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { highlightedStateTrigger, showStateTrigger } from '../animations';
+import { checkButtonTrigger, highlightedStateTrigger, showStateTrigger } from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger, showStateTrigger]
+  animations: [
+    highlightedStateTrigger, 
+    showStateTrigger, 
+    checkButtonTrigger
+  ]
 })
 
 export class ListaTarefasComponent implements OnInit {
@@ -19,6 +23,7 @@ export class ListaTarefasComponent implements OnInit {
   categoria: string = '';
   validado: boolean = false;
   indexTarefa = -1;
+  id: number = 0;
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -111,6 +116,7 @@ export class ListaTarefasComponent implements OnInit {
   }
 
   finalizarTarefa(id: number) {
+    this.id = id
     this.service.buscarPorId(id!).subscribe((tarefa) => {
       this.service.atualizarStatusTarefa(tarefa).subscribe(() => {
         this.listarAposCheck();
